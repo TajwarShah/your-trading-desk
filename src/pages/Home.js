e54bd14caaef2b1aee6c3f7101725e71b3a58ce8
= <=>
2023-09-20 21:38:54 +0500

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,499 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <section className="BannerSlider">
+        <div className="container-fluid">
+          <div id="banner_slide" className="owl-carousel owl-theme">
+            <div className="item">
+              <div className="row align-items-center">
+                <div className="col-lg-8 col-md-8 col-sm-12">
+                  <div className="BannerContent">
+                    <h1>
+                      Best Way To Buy And Sell <br /> Your Trades
+                    </h1>
+                    <p>
+                      We will show you the best way to be an option in selling
+                      and buying Forex assets
+                    </p>
+                    <div className="theme-btn">
+                      <a className="vc_general" href="#">
+                        See Details <img src="assets/images/btn-bf.png" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-12">
+                  <div className="BannerImage">
+                    <img src="assets/images/banner-img.png" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div className="row align-items-center">
+                <div className="col-lg-8 col-md-8 col-sm-12">
+                  <div className="BannerContent">
+                    <h1>
+                      Best Way To Buy And Sell <br /> Your Trades
+                    </h1>
+                    <p>
+                      We will show you the best way to be an option in selling
+                      and buying Forex assets
+                    </p>
+                    <div className="theme-btn">
+                      <a className="vc_general" href="#">
+                        See Details <img src="assets/images/btn-bf.png" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-12">
+                  <div className="BannerImage">
+                    <img src="assets/images/banner-img.png" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="OurBestPlatform">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-md-6 col-sm-12">
+              <div className="PlatformImg">
+                <img src="assets/images/platform-img.png" />
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-12">
+              <div className="PlatformCon">
+                <h2 className="theme-h2">Development Of Our Best Platform</h2>
+                <div id="projectFacts" className="sectionclassName">
+                  <div className="fullWidth eight columns">
+                    <div className="projectFactsWrap ">
+                      <div
+                        className="item wow fadeInUpBig animated animated"
+                        data-number="12"
+                        style={{ visibility: 'visible ' }}
+                      >
+                        <p>Our Users</p>
+                        <p id="number1" className="number">
+                          12 <span>M+</span>
+                        </p>
+                      </div>
+                      <div
+                        className="item wow fadeInUpBig animated animated"
+                        data-number="55"
+                        style={{ visibility: 'visible ' }}
+                      >
+                        <p>Active Transaction</p>
+                        <p id="number2" className="number">
+                          55
+                        </p>
+                      </div>
+                      <div
+                        className="item wow fadeInUpBig animated animated"
+                        data-number="359"
+                        style={{ visibility: 'visible ' }}
+                      >
+                        <p>Brand Partner</p>
+                        <p id="number3" className="number">
+                          359
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <p>
+                  Has become the best choice to become a platform that <br />{' '}
+                  accommodates users in asset transactions
+                </p>
+                <a href="#">Let’s know more</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="BestFeatures">
+        <div className="container">
+          <div className="section-title">
+            <div className="row align-items-center">
+              <div className="col-lg-8 col-md-8 col-sm-12">
+                <h2 className="theme-h2">
+                  Best Features For Users Convenience
+                </h2>
+              </div>
+              <div className="col-lg-4 col-md-4 col-sm-12">
+                <p className="theme-para">
+                  Here our features that we present to make it easier for each
+                  of our users <a href="#">Let’s know more</a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-4 col-md-4 col-sm-12">
+              <div className="FeatureBox">
+                <div className="FeatureImg">
+                  <img src="assets/images/payment.png" />
+                </div>
+                <div className="FeatureCon">
+                  <h3>Easy Payment</h3>
+                  <p>
+                    Easier payments with many options to sell or buy in our
+                    transactions
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-4 col-sm-12">
+              <div className="FeatureBox">
+                <div className="FeatureImg">
+                  <img src="assets/images/security.png" />
+                </div>
+                <div className="FeatureCon">
+                  <h3>High Security</h3>
+                  <p>
+                    Security system with high technology and encryption of every
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-4 col-sm-12">
+              <div className="FeatureBox">
+                <div className="FeatureImg">
+                  <img src="assets/images/ecosystem.png" />
+                </div>
+                <div className="FeatureCon">
+                  <h3>Best Ecosystem</h3>
+                  <p>
+                    The services we provide with full support for the
+                    convenience of our
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="VideoSec">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-md-6 col-sm-12">
+              <div className="PlatformCon">
+                <h2 className="theme-h2">
+                  Learn More Tips In Our Transactions
+                </h2>
+                <p>
+                  We provide various tutorials in the form of videos <br />{' '}
+                  specifically for our users to be able to maximize our
+                </p>
+                <div className="theme-btn">
+                  <a className="vc_general" href="#">
+                    See Details <img src="assets/images/btn-bf02.png" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-12">
+              <div className="PlatformImg">
+                <img src="assets/images/tips-img.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="CurrencySec">
+        <div className="container">
+          <div className="section-title">
+            <h2 className="theme-h2">Top Forex Currency</h2>
+            <p className="theme-para">
+              Here are the most popular forex assets and you must have for
+              investment <a href="#">Let’s know more</a>
+            </p>
+          </div>
+        </div>
+        <div className="container-fluid">
+          <div id="currency_slider" className="owl-carousel owl-theme">
+            <div className="item">
+              <div className="currencybox">
+                <div className="coin-img">
+                  <img src="assets/images/bitcpin.png" />
+                </div>
+                <div className="coin-con">
+                  <h3>Bitcoin</h3>
+                  <p className="price">
+                    USD 34879.4 <span className="diff-color pink">-5.48%</span>
+                  </p>
+                  <img src="assets/images/p-vector.png" />
+                  <a href="#">Learn More</a>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div className="currencybox">
+                <div className="coin-img">
+                  <img src="assets/images/ethereum.png" />
+                </div>
+                <div className="coin-con">
+                  <h3>Ethereum</h3>
+                  <p className="price">
+                    USD 34879.4 <span className="diff-color blue">-3.86%</span>
+                  </p>
+                  <img src="assets/images/b-vector.png" />
+                  <a href="#">Learn More</a>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div className="currencybox">
+                <div className="coin-img">
+                  <img src="assets/images/solana.png" />
+                </div>
+                <div className="coin-con">
+                  <h3>Solana</h3>
+                  <p className="price">
+                    USD 34879.4 <span className="diff-color pink">-5.48%</span>
+                  </p>
+                  <img src="assets/images/p-vector.png" />
+                  <a href="#">Learn More</a>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div className="currencybox">
+                <div className="coin-img">
+                  <img src="assets/images/bnb.png" />
+                </div>
+                <div className="coin-con">
+                  <h3>BNB</h3>
+                  <p className="price">
+                    USD 34879.4 <span className="diff-color blue">-5.48%</span>
+                  </p>
+                  <img src="assets/images/b-vector.png" />
+                  <a href="#">Learn More</a>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div className="currencybox">
+                <div className="coin-img">
+                  <img src="assets/images/bnb.png" />
+                </div>
+                <div className="coin-con">
+                  <h3>BNB</h3>
+                  <p className="price">
+                    USD 34879.4 <span className="diff-color pink">-5.48%</span>
+                  </p>
+                  <img src="assets/images/b-vector.png" />
+                  <a href="#">Learn More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="slideState">
+            <span></span>
+          </div>
+        </div>
+      </section>
+
+      <section className="TestimonialSec">
+        <div className="container">
+          <div className="section-title">
+            <h2 className="theme-h2">
+              Testimonials From Our <br /> Platform Users
+            </h2>
+          </div>
+          <div id="testimonial_slider" className="owl-carousel owl-theme">
+            <div className="item">
+              <div className="testimonialmain">
+                <div className="star-rating">
+                  <ul>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+                <div className="FirstPara">
+                  <p>
+                    Is the best crypto asset trading <br /> platform that I have
+                    ever used,{' '}
+                  </p>
+                </div>
+                <div className="testimonialbox">
+                  <div className="testimonial-img">
+                    <img src="assets/images/Memoji.png" />
+                  </div>
+                  <div className="testimonial-con">
+                    <h3>Christopher Smith</h3>
+                    <p className="designation">Investor</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonialmain">
+                <div className="star-rating">
+                  <ul>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+                <div className="FirstPara">
+                  <p>
+                    Is the best crypto asset trading <br /> platform that I have
+                    ever used,{' '}
+                  </p>
+                </div>
+                <div className="testimonialbox">
+                  <div className="testimonial-img">
+                    <img src="assets/images/Memoji.png" />
+                  </div>
+                  <div className="testimonial-con">
+                    <h3>Christopher Smith</h3>
+                    <p className="designation">Investor</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonialmain">
+                <div className="star-rating">
+                  <ul>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+                <div className="FirstPara">
+                  <p>
+                    Is the best crypto asset trading <br /> platform that I have
+                    ever used,{' '}
+                  </p>
+                </div>
+                <div className="testimonialbox">
+                  <div className="testimonial-img">
+                    <img src="assets/images/Memoji.png" />
+                  </div>
+                  <div className="testimonial-con">
+                    <h3>Christopher Smith</h3>
+                    <p className="designation">Investor</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonialmain">
+                <div className="star-rating">
+                  <ul>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                    <li>
+                      <i className="fas fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+                <div className="FirstPara">
+                  <p>
+                    Is the best crypto asset trading <br /> platform that I have
+                    ever used,{' '}
+                  </p>
+                </div>
+                <div className="testimonialbox">
+                  <div className="testimonial-img">
+                    <img src="assets/images/Memoji.png" />
+                  </div>
+                  <div className="testimonial-con">
+                    <h3>Christopher Smith</h3>
+                    <p className="designation">Investor</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="NewsletterSec">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-2 col-lg-2 col-sm-12"></div>
+            <div className="col-lg-8 col-lg-8 col-sm-12">
+              <div className="NewsletterCon">
+                <h2 className="theme-h2">Join our news letter</h2>
+                <p>
+                  Let's join our platform community and start investing in
+                  cryptocurrency
+                </p>
+                <form action="index.php">
+                  <input type="email" placeholder="Enter your email" />
+                  <input
+                    className="btn-primary"
+                    type="submit"
+                    value="Contact us"
+                  />
+                </form>
+              </div>
+            </div>
+            <div className="col-lg-2 col-lg-2 col-sm-12"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;
